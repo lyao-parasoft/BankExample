c9ee714b9f083a3e0ee74ebf0eb781fa4fee48e5
Alfie Wang <awang@parasoft.com>
2023-07-14 11:25:54 +0800

    generate-coverage_192.168.3.28.bat edited online with Bitbucket
    Add dotnet to path.

--- generate-coverage_192.168.3.28.bat
+++ generate-coverage_192.168.3.28.bat
@@ -5,4 +5,5 @@ rem CMD: `generate-coverage_192.168.3.28.bat`
 rem Git Bash: `./generate-coverage_192.168.3.28.bat`
 rem PowerShell: `.\generate-coverage_192.168.3.28.bat`
 
+set "Path=C:\Program Files\dotnet;%Path%"
 "D:\Programs\Parasoft\parasoft_dottest_2023.1.0\dottest\dottestcli.exe" -solution "BankExample.NET.WithTests.sln" -config "builtin://Run VSTest Tests with Coverage" -settings "localsettings.properties" -report "build/reports/dottest/ut"
