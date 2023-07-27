4f7fbd069857cfb3d610df476aa33bbc05ba38a5
Alfie Wang <awang@parasoft.com>
2023-07-27 10:59:22 +0800

    Fix the problem of not generating coverage report when run on Jenkins remote agent.

--- generate-coverage_192.168.3.28.bat
+++ generate-coverage_192.168.3.28.bat
@@ -5,4 +5,11 @@ rem CMD: `generate-coverage_192.168.3.28.bat`
 rem Git Bash: `./generate-coverage_192.168.3.28.bat`
 rem PowerShell: `.\generate-coverage_192.168.3.28.bat`
 
+if "%OS%"=="Windows_NT" setlocal
+
+rem The following line is necessary for when run this on Jenkins remote agent.
+set "ProgramData=C:\ProgramData"
+
 "D:\Programs\Parasoft\parasoft_dottest_2023.1.0\dottest\dottestcli.exe" -solution "BankExample.NET.WithTests.sln" -config "builtin://Run VSTest Tests with Coverage" -settings "localsettings.properties" -report "build/reports/dottest/ut"
+
+if "%OS%"=="Windows_NT" endlocal
