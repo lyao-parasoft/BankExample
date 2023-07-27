11cb65b33095e5a36376a58d417ca33f770e89cd
Alfie Wang <awang@parasoft.com>
2023-07-27 11:17:39 +0800

    Add additional env vars required for Jenkins remote agent.

--- generate-coverage_192.168.3.28.bat
+++ generate-coverage_192.168.3.28.bat
@@ -7,8 +7,10 @@ rem PowerShell: `.\generate-coverage_192.168.3.28.bat`
 
 if "%OS%"=="Windows_NT" setlocal
 
-rem The following line is necessary for when run this on Jenkins remote agent.
+rem The following lines are necessary for when run this on Jenkins remote agent.
+set "APPDATA=C:\Users\devtest\AppData\Roaming"
 set "ProgramData=C:\ProgramData"
+set "USERPROFILE=C:\Users\devtest"
 
 "D:\Programs\Parasoft\parasoft_dottest_2023.1.0\dottest\dottestcli.exe" -solution "BankExample.NET.WithTests.sln" -config "builtin://Run VSTest Tests with Coverage" -settings "localsettings.properties" -report "build/reports/dottest/ut"
 
