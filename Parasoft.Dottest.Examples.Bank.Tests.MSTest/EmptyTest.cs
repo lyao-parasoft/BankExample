803bb180e3400884bd4580f825f1c48a61a79886
lli-parasoft <lli-parasoftA@Email.com>
2023-10-27 16:07:58 +0800

    Modify code coverage.

---
+++ Parasoft.Dottest.Examples.Bank.Tests.MSTest/EmptyTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parasoft.Dottest.Examples.Bank.Tests.MSTest
+{
+    [TestClass]
+    public class EmptyTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
